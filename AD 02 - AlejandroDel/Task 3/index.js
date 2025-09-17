51e68f60b4965167338317343351b36fbe947ce3
AlejandroDel <alejandrohdzd@proton.me>
2025-09-17 14:46:27 -0600

    Add files via upload

---
+++ AD 02 - AlejandroDel/Task 3/index.js
@@ -0,0 +1,15 @@
+function cambiarEncabezado()
+{
+    let heading = document.getElementById("h1");
+    heading.innerHTML = "Adios";
+}
+
+function cambiarColor1() {
+    document.getElementById('h2')
+        .style = "color:orange;";
+}
+
+function cambiarColor2() {
+    document.getElementById('h3')
+        .style = "color:brown;";
+}
